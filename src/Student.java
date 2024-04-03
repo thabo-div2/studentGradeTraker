6b98ae7274eb462f30ddd62caa5d3a17ba8f1847
Thabo Setsubi <thabosetsubi3@gmail.com>
2024-04-03 09:27:01 +0200

    intiail commit

---
+++ src/Student.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Thabo Setsubi st10445734@vcconnnect.edu.za ST10445734
+ */
+public class Student
+{
+    String name;
+    float mark;
+    Student() {
+    }
+    
+    Student(String name, float mark) {
+        this.name = name;
+        this.mark = mark;
+    }
+    
+    
+    //-------------------------00ooo0oo End of file oo0ooo00------------------//
+}
